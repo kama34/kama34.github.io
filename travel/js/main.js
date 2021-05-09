63c07d8448d65baff5f4104a8d3c8e5c5c5b8192
kama34 <62620760+kama34@users.noreply.github.com>
2021-05-10 02:54:08 +0300

    Add files via upload

--- travel/js/main.js
+++ travel/js/main.js
@@ -14,3 +14,19 @@ function play(){
     document.getElementById('video').innerHTML = '<iframe  class="video__element" src="https://www.youtube.com/embed/URDXZSJZ2ME?autoplay=1" title="" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
     video.classList.add('video__item__active');
 }
+
+// Burger handler
+
+(function(){
+    const burgerItem = document.querySelector('.burger');
+    const menu = document.querySelector('.header__nav');
+    const close = document.querySelector('.header__nav-close');
+    
+    burgerItem.addEventListener('click', () => {
+        menu.classList.add('header__nav__active');
+    });
+
+    close.addEventListener('click', () => {
+        menu.classList.remove('header__nav__active');
+    });
+}());
