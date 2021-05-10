5369d32345390e5601b74c776d6b40baf873d3ef
kama34 <62620760+kama34@users.noreply.github.com>
2021-05-10 04:39:51 +0300

    Add files via upload

---
+++ travel/js/main-es6.js
@@ -0,0 +1,78 @@
+(function () {
+    const header = document.querySelector('.header');
+    window.onscroll = () => {
+        if (window.pageYOffset > 50) {
+            header.classList.add('header__active');
+        } else {
+            header.classList.remove('header__active');
+        }
+    };
+} ());
+
+function play(){
+    const video = document.querySelector('.video__item');
+    document.getElementById('video').innerHTML = '<iframe  class="video__element" src="https://www.youtube.com/embed/URDXZSJZ2ME?autoplay=1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+    video.classList.add('video__item__active');
+}
+
+// Burger handler
+(function(){
+    const burgerItem = document.querySelector('.burger');
+    const menu = document.querySelector('.header__nav');
+    const close = document.querySelector('.header__nav-close');
+    const menuLinks = document.querySelectorAll('.header__link');
+
+    burgerItem.addEventListener('click', () => {
+        menu.classList.add('header__nav__active');
+    });
+
+    close.addEventListener('click', () => {
+        menu.classList.remove('header__nav__active');
+    });
+    if (window.innerWidth < 768) {
+        for (let i = 0; i < menuLinks.length; i++){
+            menuLinks[i].addEventListener('click', () => {
+                menu.classList.remove('header__nav__active');
+            });
+        }
+    }
+}());
+
+// Scroll to anchors
+(function(){
+    const smoothScroll = function (targetEl, duration){
+        const headrElHeight = document.querySelector('.header').clientHeight;
+        let target = document.querySelector(targetEl);
+        let targetPosition = target.getBoundingClientRect().top - headrElHeight;
+        let startPosition = window.pageYOffset;
+        let startTime = null;
+
+        const ease = function(t,b,c,d){
+            t /= d / 2;
+            if (t < 1) return c / 2 * t * t +b;
+            t--;
+            return -c / 2 * (t * (t - 2) - 1) + b;
+        };
+
+        const animation = function(currentTime){
+            if (startTime == null) startTime = currentTime;
+            const timeElapsed = currentTime - startTime;
+            const run = ease(timeElapsed, startPosition, targetPosition, duration);
+            window.scrollTo(0,run);
+            if (timeElapsed < duration) requestAnimationFrame(animation);
+        };
+
+        requestAnimationFrame(animation);
+    };
+
+    const scrollTo = function() {
+        const links = document.querySelectorAll('.js-scroll');
+        links.forEach(each => {
+            each.addEventListener('click', function() {
+                const currentTarget = this.getAttribute('href');
+                smoothScroll(currentTarget, 1000);
+            });
+        });
+    };
+    scrollTo();
+}());
